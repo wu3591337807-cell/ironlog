393b37e1a48dc64a06599c3a3d88ec8add0cff9f
wu3591337807-cell <wu3591337807@gmail.com>
2026-06-11 11:00:49 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
-const CACHE='ironlog-v1';
+const CACHE='ironlog-20260611';
 self.addEventListener('install',function(e){self.skipWaiting()});
-self.addEventListener('activate',function(e){e.waitUntil(self.clients.claim())});
+self.addEventListener('activate',function(e){e.waitUntil(
+  caches.keys().then(function(ks){return Promise.all(ks.map(function(k){if(k!==CACHE)return caches.delete(k)}))}).then(function(){return self.clients.claim()})
+)});
 self.addEventListener('fetch',function(e){
   if(e.request.method!=='GET')return;
   e.respondWith(
